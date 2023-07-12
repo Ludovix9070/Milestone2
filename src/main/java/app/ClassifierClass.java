4764fa92a99934858d8052cf6c85e0a35118505b
ludovico <ludovix9070@gmail.com>
2023-07-13 00:53:08 +0200

    Second Milestone

---
+++ src/main/java/app/ClassifierClass.java
@@ -0,0 +1,69 @@
+package app;
+
+import app.control.RetrieveWekaInfo;
+import app.files.EvaluationFile;
+import app.model.AllEvaluationLists;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.logging.Logger;
+
+public class ClassifierClass {
+    private static final Logger LOGGER = Logger.getLogger(ClassifierClass.class.getName());
+
+    public static void main(String[] args) throws Exception {
+        String bookkeeperPath = "BookkeeperWalkForward";
+        String syncopePath = "SyncopeWalkForward";
+
+        WalkForward.startWalkForward(bookkeeperPath,syncopePath);
+
+        String projName = "bookkeeper";
+        RetrieveWekaInfo retWekaInfo = new RetrieveWekaInfo(bookkeeperPath+"/", 7);
+        AllEvaluationLists allLists = retWekaInfo.retrieveClassifiersEvaluation(projName);
+
+        EvaluationFile evaluationFileAvg = new EvaluationFile(projName, allLists.getAvgEvaluationsList(), "avg");
+        evaluationFileAvg.csvWrite(projName);
+        EvaluationFile evaluationFileDetails = new EvaluationFile(projName, allLists.getMergeEvaluationsList(), "details");
+        evaluationFileDetails.csvWrite(projName);
+
+        projName = "syncope";
+        retWekaInfo = new RetrieveWekaInfo(syncopePath+"/", 30);
+        try{
+            allLists = retWekaInfo.retrieveClassifiersEvaluation(projName);
+        } catch(Exception e){
+
+        }
+
+        //allLists = retWekaInfo.retrieveClassifiersEvaluation(projName);
+
+
+        evaluationFileAvg = new EvaluationFile(projName, allLists.getAvgEvaluationsList(), "avg");
+        evaluationFileAvg.csvWrite(projName);
+        evaluationFileDetails = new EvaluationFile(projName, allLists.getMergeEvaluationsList(), "details");
+        evaluationFileDetails.csvWrite(projName);
+
+        cleanUp(bookkeeperPath);
+        cleanUp(syncopePath);
+    }
+
+    public static void cleanUp(String path) {
+        try {
+            Files.walkFileTree(Paths.get(path), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    Files.delete(dir);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (Exception e){
+            LOGGER.severe(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/app/WalkForward.java
@@ -0,0 +1,110 @@
+package app;
+
+import java.io.*;
+import java.util.logging.Logger;
+
+public class WalkForward {
+    private WalkForward() {
+        throw new IllegalStateException("Utility class");
+    }
+    private static final Logger LOGGER = Logger.getLogger(WalkForward.class.getName());
+    private static final String PATH_DELIMITER = "/";
+
+    private static void writeArffLine(FileWriter fileWriter, String[] val) throws IOException {
+        for(int i = 3; i < val.length; i++){
+            fileWriter.append(val[i]);
+            if(i != val.length-1){
+                fileWriter.append(",");
+            }
+        }
+        fileWriter.append("\n");
+    }
+
+    private static void arffInit(FileWriter wr, String name) throws IOException {
+        wr.write("@RELATION " + name + "\n\n");
+        wr.write("@ATTRIBUTE Age numeric\n");
+        wr.write("@ATTRIBUTE Revisions numeric\n");
+        wr.write("@ATTRIBUTE Bugfix numeric\n");
+        wr.write("@ATTRIBUTE LOCs numeric\n");
+        wr.write("@ATTRIBUTE LOCs_Touched numeric\n");
+        wr.write("@ATTRIBUTE LOCs_Added numeric\n");
+        wr.write("@ATTRIBUTE Churn numeric\n");
+        wr.write("@ATTRIBUTE Avg_Churn numeric\n");
+        wr.write("@ATTRIBUTE Authors_Number numeric\n");
+        wr.write("@ATTRIBUTE Average_Change_Set numeric\n");
+        wr.write("@ATTRIBUTE Buggy {false,true}\n\n");
+        wr.write("@DATA\n");
+    }
+
+    public static int getMaxReleaseNumber(FileReader fr) {
+        try (BufferedReader br = new BufferedReader(fr)) {
+            String line;
+            String lastLine = null;
+            while ((line = br.readLine()) != null) {
+                lastLine = line;
+            }
+            assert lastLine != null;
+            return Integer.parseInt(lastLine.split(",")[0]);
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    private static void singlewalkForward(String inputFilePath, int index, FileWriter fileWriterTrain, FileWriter fileWriterTest){
+        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split(",");
+                if (!values[0].equals("Version")) {
+                    if (Integer.parseInt(values[0]) == index) {
+                        // csv di testing
+                        writeArffLine(fileWriterTest, values);
+                    } else if (Integer.parseInt(values[0]) < index) {
+                        // csv di training
+                        writeArffLine(fileWriterTrain, values);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.severe(e.getMessage());
+        }
+    }
+    
+    private static void walkForward(String outputDirectoryPath, String inputFilePath){
+
+        try {
+            String outputFilePathTrain;
+            String outputFilePathTest;
+
+            for(int index = 2; index <= getMaxReleaseNumber(new FileReader(inputFilePath)); index++){
+                outputFilePathTrain = outputDirectoryPath + PATH_DELIMITER + index + PATH_DELIMITER + "Train.arff";
+                outputFilePathTest = outputDirectoryPath + PATH_DELIMITER + index + PATH_DELIMITER + "Test.arff";
+                new File(outputDirectoryPath + PATH_DELIMITER + index).mkdir();
+
+                FileWriter fileWriterTrain = new FileWriter(outputFilePathTrain);
+                arffInit(fileWriterTrain, "Train");
+                FileWriter fileWriterTest = new FileWriter(outputFilePathTest);
+                arffInit(fileWriterTest, "Test");
+
+                singlewalkForward(inputFilePath, index, fileWriterTrain, fileWriterTest);
+
+                fileWriterTrain.close();
+                fileWriterTest.close();
+            }
+        } catch (Exception e) {
+            LOGGER.severe(e.getMessage());
+        }
+    }
+
+    public static void startWalkForward(String bookkeeperOutputPath, String syncopeOutputPath) {
+        String bookkeeperFilePath = "./BOOKKEEPER.csv";
+        new File(bookkeeperOutputPath).mkdir();
+        walkForward(bookkeeperOutputPath, bookkeeperFilePath);
+
+        String syncopeFilePath = "./SYNCOPEb.csv";
+        new File(syncopeOutputPath).mkdir();
+        walkForward(syncopeOutputPath, syncopeFilePath);
+    }
+}
